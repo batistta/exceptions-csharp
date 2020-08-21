ff3a22b95a4f587250e959381e252bd63853ed15
batistta <assuerio12@gmail.com>
2020-08-20 21:32:48 -0300

    initial commit

---
+++ tratamento de exceções/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tratamento_de_exceções
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    int A, B;
+                    Console.Write("\nDivisão de A por B\nDigite A:");
+                    A = int.Parse(Console.ReadLine());
+                    Console.Write("\nDigite B:");
+                    B = int.Parse(Console.ReadLine());
+                    Console.WriteLine(A / B);
+                }
+                catch (DivideByZeroException X)
+                {
+                    Console.WriteLine(X.Message);
+                }
+                catch (FormatException lul)
+                {
+                    Console.WriteLine(lul.Message);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
